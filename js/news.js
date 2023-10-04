d1753744e6ce9d4557d0bad6e84492dc3bd89f96
rkr-ksk <145410750+rkr-ksk@users.noreply.github.com>
2023-10-04 13:30:49 +0900

    Add files via upload

--- js/news.js
+++ js/news.js
@@ -33,4 +33,28 @@
     len = 80; // リセット
   };
 
+  // 関連記事の字数制限
+  const rpLimit = document.querySelectorAll(".rp-abstract");
+  let rpLen = 60; // 半角60字（全角約30字）
+
+  for (let i=0; i < rpLimit.length; i++) {
+    str = rpLimit[i].textContent;
+    // 1400px未満かつSP幅ではない場合50文字に設定。動的には変化しないためリサイズ後に表示を調整したい場合はリロードが必要
+    if (windowWidth < 1400 && 1080 < windowWidth) {
+      rpLen = 50;
+    }
+    if (str.length > rpLen) {
+      rpLimit[i].textContent = str.substring(0, rpLen) + "…";
+    }
+    rpLen = 60; // リセット
+  };
+
+  // SP表示ではSNS名を省略
+  const snsName = document.querySelectorAll(".sns-name");
+
+  for (let i=0; i < snsName.length; i++) {
+    if (windowWidth < 768) {
+      snsName[i].textContent = '';
+    }
+  }
 }
