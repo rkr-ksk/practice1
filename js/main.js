61d766f814e1f2099b7553c3bf2cd53102b46e66
rkr-ksk <145410750+rkr-ksk@users.noreply.github.com>
2023-10-05 14:56:29 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -193,5 +193,43 @@
     });
   }
 
+  // ポップアップウィンドウ設定
+  window.addEventListener('load', function() {
+    // 初回のみ1秒後に表示
+    if (!getCookie('visited')) {
+      setTimeout(function() {
+        document.getElementById('modal').style.display = 'block';
+      }, 1000);
+      // Cookieを設定
+      setCookie('visited', 'true', 1);
+    }
+  });
+  
+  document.getElementById('modal').addEventListener('click', function() {
+    this.style.display = 'none';
+  });
+
+  // Cookieを制御
+  function setCookie(name, value, days) {
+    var expires = '';
+    if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      expires = '; expires=' + date.toUTCString();
+    }
+    document.cookie = name + '=' + value + expires + '; path=/';
+  }
+
+  function getCookie(name) {
+    var nameEQ = name + '=';
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+  }
+
 
 }
